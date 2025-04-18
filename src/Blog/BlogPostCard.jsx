1ec19f6b3f270f7f8eaa6a7c9a66e5a86c7b3f19
Nicolas Murphy <murphynick10@gmail.com>
2025-04-18 05:08:17 -0500

    refactored blogs

--- src/Blog/BlogPostCard.jsx
+++ src/Blog/BlogPostCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function BlogPostCard({ post }) {
   return (
-    <div className="fade-in-2 card bg-base-100 shadow-xl">
+    <div className="fade-in-2 card bg-base-100 shadow-xl mb-4">
       <div className="card-body items-center text-center">
         <h2 className="card-title mb-[2%] text-3xl">{post.title}</h2>
         <p className="text-sm text-base-content mb-[2%]">{post.summary}</p>
---
+++ src/Blog/BlogPosts.js
@@ -0,0 +1,17 @@
+import ExploringProteinMusic from "./ExploringProteinMusic";
+import Microtonality from "./Microtonality";
+
+export const blogPosts = [
+  {
+    slug: "exploring-protein-music",
+    title: "Exploring Protein Music",
+    summary: "Turning spectra data into audio",
+    component: ExploringProteinMusic,
+  },
+//   {
+//     slug: "microtonality",
+//     title: "Microtonality",
+//     summary: "The future of music",
+//     component: Microtonality,
+//   },
+];
